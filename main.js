3e22f7e41300e37e67194ee4906c937547db96ed
ned eisenberg <ned.eisenberg@gmail.com>
2018-05-27 18:50:24 -0400

    hook tag

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ video_column = document.createElement('div');
 video_column.setAttribute('id','video_column');
 video_column.innerHTML = "check";
 
-jQuery("#transcript2vtt").append(video_column);
+jQuery("#hook_tag").append(video_column);
 //document.body.append(video_column);
