ec5898379e24651f3a3dc28152de44e86047e50f
ned eisenberg <ned.eisenberg@gmail.com>
2018-05-27 19:09:41 -0400

    function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+function run{
 video_column = document.createElement('div');
 video_column.setAttribute('id','hook_tag');
 video_column.innerHTML = "check";
-
+};
 //jQuery("#hook_tag").append(video_column);
 //document.body.append(video_column);
