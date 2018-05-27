855f07bebf495f2c40c10e2a08ffe7ab326eddcd
ned eisenberg <ned.eisenberg@gmail.com>
2018-05-27 18:29:39 -0400

    update main.js hook functino

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ video_column = document.createElement('div');
 video_column.setAttribute('id','video_column');
 video_column.innerHTML = "check";
 
-document.body.append(video_column);
+jQuery("#transcript2vtt").append(video_column);
+//document.body.append(video_column);
