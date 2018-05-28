958bf3320a495cbc741103f67366fbbeba2cdcc8
ned eisenberg <ned.eisenberg@gmail.com>
2018-05-27 20:15:52 -0400

    check mate

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-function run(){
-  video_column = document.createElement('div');
-  video_column.setAttribute('id','hook_tag');
-  video_column.innerHTML = "check";
-}
-//document.body.append(video_column);
-//run();
-console.log('duh');
 video_column = document.createElement('div');
 video_column.setAttribute('id','hook_tag');
-video_column.innerHTML = "check";
+video_column.innerHTML = "check mate";
 jQuery("#content").append(video_column);
